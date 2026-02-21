bf11377f371d40cb9194296cdb142ccb4f97adfe
Gleb Ivanov <glivanov@samokat.ru>
2026-02-21 21:03:28 +0300

    Add type specs for proto records

--- apps/proto/include/proto.hrl
+++ apps/proto/include/proto.hrl
@@ -1,12 +1,22 @@
--record(message, {author, text}).
+-record(message, {
+    author :: binary(),
+    text :: binary()
+}).
 
--record(auth, {seq_id, login, password}).
--record(send_message, {seq_id, text}).
+-record(auth, {
+    seq_id :: pos_integer(),
+    login :: binary(),
+    password :: binary()
+}).
+-record(send_message, {
+    seq_id :: pos_integer(),
+    text :: binary()
+}).
 
--record(auth_ok, {seq_id}).
--record(auth_error, {seq_id}).
--record(already_connected, {seq_id}).
--record(message_sent, {seq_id}).
+-record(auth_ok, {seq_id :: pos_integer()}).
+-record(auth_error, {seq_id :: pos_integer()}).
+-record(already_connected, {seq_id :: pos_integer()}).
+-record(message_sent, {seq_id :: pos_integer()}).
 
 -type proto_entity() :: event() | req() | resp().
 
