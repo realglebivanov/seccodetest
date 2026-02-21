6b03c7f45acb78572b1b18029c034daf6e58f00d
Gleb Ivanov <glivanov@samokat.ru>
2026-02-21 21:03:59 +0300

    Reorganize proto parsing code

--- apps/proto/src/proto.erl
+++ apps/proto/src/proto.erl
@@ -23,59 +23,65 @@ next_seq_id() ->
 
 -spec encode(event() | req() | resp()) -> binary().
 encode(#message{author = Author, text = Text}) ->
-    json:encode(#{type => <<"message">>, author => Author, text => Text});
+    json:encode(#{type => ~"message", author => Author, text => Text});
 encode(#send_message{seq_id = SeqId, text = Text}) ->
-    json:encode(#{seq_id => SeqId, type => <<"send_message">>, text => Text});
+    json:encode(#{seq_id => SeqId, type => ~"send_message", text => Text});
 encode(#message_sent{seq_id = SeqId}) ->
-    json:encode(#{seq_id => SeqId, type => <<"message_sent">>});
+    json:encode(#{seq_id => SeqId, type => ~"message_sent"});
 encode(#auth{seq_id = SeqId, login = Login, password = Password}) ->
-    json:encode(#{seq_id => SeqId, type => <<"auth">>, login => Login, password => Password});
+    json:encode(#{seq_id => SeqId, type => ~"auth", login => Login, password => Password});
 encode(#auth_ok{seq_id = SeqId}) ->
-    json:encode(#{seq_id => SeqId, type => <<"auth_ok">>});
+    json:encode(#{seq_id => SeqId, type => ~"auth_ok"});
 encode(#auth_error{seq_id = SeqId}) ->
-    json:encode(#{seq_id => SeqId, type => <<"auth_error">>});
+    json:encode(#{seq_id => SeqId, type => ~"auth_error"});
 encode(#already_connected{seq_id = SeqId}) ->
-    json:encode(#{seq_id => SeqId, type => <<"already_connected">>}).
+    json:encode(#{seq_id => SeqId, type => ~"already_connected"}).
 
 -spec decode(binary()) -> {ok, proto_entity()} | error.
 decode(Bin) ->
     try json:decode(Bin) of
-        Data -> from_data(Data)
+        #{~"type" := ~"message"} = Data ->
+            decode_event(Data);
+        #{~"seq_id" := SeqId} = Data when is_integer(SeqId) andalso SeqId > 0 ->
+            decode_reqresp(Data);
+        _ ->
+            error
     catch
         _:_Reason -> error
     end.
 
--spec from_data(json:decode_value()) -> {ok, proto_entity()} | error.
-from_data(#{
-    <<"type">> := <<"message">>,
-    <<"author">> := Author,
-    <<"text">> := Text
-}) when
-    is_binary(Author) and is_binary(Text)
-->
+-spec decode_event(json:decode_value()) -> {ok, event()} | error.
+decode_event(#{
+    ~"type" := ~"message",
+    ~"author" := Author,
+    ~"text" := Text
+}) when is_binary(Author) and is_binary(Text) ->
     {ok, #message{author = Author, text = Text}};
-from_data(#{
-    <<"seq_id">> := SeqId,
-    <<"type">> := <<"send_message">>,
-    <<"text">> := Text
+decode_event(_) ->
+    error.
+
+-spec decode_reqresp(json:decode_value()) -> {ok, req() | resp()} | error.
+
+decode_reqresp(#{
+    ~"seq_id" := SeqId,
+    ~"type" := ~"send_message",
+    ~"text" := Text
 }) when is_binary(Text) ->
     {ok, #send_message{seq_id = SeqId, text = Text}};
-from_data(#{<<"seq_id">> := SeqId, <<"type">> := <<"message_sent">>}) ->
+decode_reqresp(#{~"seq_id" := SeqId, ~"type" := ~"message_sent"}) ->
     {ok, #message_sent{seq_id = SeqId}};
-from_data(#{
-    <<"seq_id">> := SeqId,
-    <<"type">> := <<"auth">>,
-    <<"login">> := Login,
-    <<"password">> := Password
-}) when
-    is_binary(Login) and is_binary(Password)
-->
+decode_reqresp(#{
+    ~"seq_id" := SeqId,
+    ~"type" := ~"auth",
+    ~"login" := Login,
+    ~"password" := Password
+}) when is_binary(Login) and is_binary(Password) ->
     {ok, #auth{seq_id = SeqId, login = Login, password = Password}};
-from_data(#{<<"seq_id">> := SeqId, <<"type">> := <<"auth_ok">>}) ->
+decode_reqresp(#{~"seq_id" := SeqId, ~"type" := ~"auth_ok"}) ->
     {ok, #auth_ok{seq_id = SeqId}};
-from_data(#{<<"seq_id">> := SeqId, <<"type">> := <<"auth_error">>}) ->
+decode_reqresp(#{~"seq_id" := SeqId, ~"type" := ~"auth_error"}) ->
     {ok, #auth_error{seq_id = SeqId}};
-from_data(#{<<"seq_id">> := SeqId, <<"type">> := <<"already_connected">>}) ->
+decode_reqresp(#{~"seq_id" := SeqId, ~"type" := ~"already_connected"}) ->
     {ok, #already_connected{seq_id = SeqId}};
-from_data(_Data) ->
+decode_reqresp(_Data) ->
     error.
